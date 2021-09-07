3b55f28e9dcca36b0d3ec25d00f1a4ee69970b02
MoatazAllam <mezo.allam2015@hotmail.com>
2021-09-07 16:47:55 +0200

    solve merge conflicts

--- API/Controllers/ErrorController.cs
+++ API/Controllers/ErrorController.cs
@@ -7,12 +7,5 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-//    [Route("errors/{code}")]
-//    public class ErrorController : ControllerBase
-//    {
-//        public IActionResult Error(int code)
-//        {
-//            return new ObjectResult(new ApiResponse(code));
-//        }
-//    }
+
 }
